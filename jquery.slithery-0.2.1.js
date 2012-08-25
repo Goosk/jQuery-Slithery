1c5e9599d1d0a1f2a1d1d667ef2b56e495bbe6c4
Goosk <admin@goosk.se>
2012-08-25 13:37:12 +0200

    jQuery Slithery 0.2.1

--- jquery.slithery-0.2.1.js
+++ jquery.slithery-0.2.1.js
@@ -1,6 +1,6 @@
 /**
- * Slithery jQuery Plugin v0.2
- * Date: 2012-03-17
+ * Slithery jQuery Plugin v0.2.1
+ * Date: 2012-08-25
  * Info: https://github.com/Goosk/jQuery-Slithery
  */
 
@@ -12,74 +12,54 @@
 				var animShow = {};
 				var animHide = {};
 				var $window = $(window);
-				var posCss, posAnim;
+				var posCss, posAnim, objSize;
 				var animating = false; // Flag to check if we're already in the middle of an animation
 				var position = obj.position()['top']; // Position of the element from the top of the page in pixels
+
+				// Settings should be defaults if none are given
+				if(typeof settings === 'undefined') {
+					settings = {};
+				}
 				
 				// Default values
-				var defaults = {
+				var vars = {
 					position: 'top',
 					easing: 'linear',
 					top: 300,
 					speed: 250
 				};
 				
-				// Settings should be defaults if none are given
-				if(typeof settings === 'undefined') {
-					settings = {};
-				}
+				$.extend(vars, settings);
+
+				// Set positions and animations so the objects will animate correctly
+				posCss = 'margin-'+ vars['position'];
+				posAnim = 'margin'+ vars['position'].charAt(0).toUpperCase() + vars['position'].slice(1);
 				
-				var vars = {};
-				
-				// Set values if they're set, otherwise fall back to defaults
-				vars['position'] = ((settings['position'] !== undefined) ? settings['position'] : defaults['position']);
-				vars['easing'] = ((settings['easing'] !== undefined) ? settings['easing'] : defaults['easing']);
-				vars['top'] = ((settings['top'] !== undefined) ? settings['top'] : defaults['top']);
-				vars['speed'] = ((settings['speed'] !== undefined) ? settings['speed'] : defaults['speed']);
-				
-				if(settings['onHide'] !== undefined) vars['onHide'] = settings['onHide'];
-				if(settings['onShow'] !== undefined) vars['onShow'] = settings['onShow'];
-				
-				// Set the right values for animations
-				switch(vars['position']) {
-					default:
-					case 'top':
-						posCss	= 'margin-top';
-						posAnim = 'marginTop';
-						objSize = obj.outerHeight();
-						break;
-						
-					case 'left':
-						posCss	= 'margin-left';
-						posAnim = 'marginLeft';
-						objSize = obj.outerWidth();
-						break;
-						
-					case 'right':
-						posCss	= 'margin-right';
-						posAnim = 'marginRight';
-						objSize = obj.outerWidth();
-						break;
-						
-					case 'bottom':
-						posCss	= 'margin-bottom';
-						posAnim = 'marginBottom';
-						objSize = obj.outerHeight();
-						break;
+				if(vars['position'] == 'top' || vars['position'] == 'bottom') {
+					objSize = obj.outerHeight();
+				} else if (vars['position'] == 'left' || vars['position'] == 'right') {
+					objSize = obj.outerWidth();
 				}
 				
 				animShow[posAnim] = [0, vars['easing']];
 				animHide[posAnim] = [-objSize, vars['easing']];
 				
 				var update = function() {
+
 					// Update position of the element
 					position = $window.scrollTop();
 					
 					// If the element is further down than we want
 					if(position > vars['top']) {
+
 						// is not in the middle of an animation and not visible
 						if(!animating && !obj.is(':visible')) {
 							animating = true;
+
+							// Trigger onShow function if set
+							if(vars['onShow'] !== undefined) {
+								vars['onShow']();
+							}
 							
 							// Show our element
 							obj.css(posCss, -objSize);
@@ -89,17 +69,23 @@
 								complete: function() {
 									animating = false;
 									
-									// Run onShow if set by user
-									if(vars['onShow'] !== undefined) {
-										vars['onShow']();
+									// Run onFullyShown if set
+									if(vars['onFullyShown'] !== undefined) {
+										vars['onFullyShown']();
 									}
 								}
 							});
 						}
 					} else if(position < (vars['top'] + 5)) {
+
 						// We should hide it if it's visible and not already being hidden
 						if(!animating && obj.is(':visible')) {
 							animating = true;
+
+							// Run onHide if set
+							if(vars['onHide'] !== undefined) {
+								vars['onHide']();
+							}
 							
 							// Hide element
 							obj.animate(animHide, {
@@ -108,9 +94,9 @@
 									obj.hide();
 									animating = false;
 									
-									// Run onHide if set by user
-									if(vars['onHide'] !== undefined) {
-										vars['onHide']();
+									// Run onHide if set
+									if(vars['onFullyHidden'] !== undefined) {
+										vars['onFullyHidden']();
 									}
 								}
 							});
